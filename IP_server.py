c12f2f21b40ba627a722136d5e01403c6599afb7
ishasave <86420660+ishasave@users.noreply.github.com>
2023-04-27 20:10:02 +0530

    Add files via upload

---
+++ IP_server.py
@@ -0,0 +1,33 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 4698
+
+def send_data(data):
+    return str(data).encode('utf-8')
+
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+    s.bind((HOST,PORT))
+    print("Server binded to host %s and port %d" %(HOST,PORT))
+    s.listen()
+    conn,address = s.accept()
+    with conn:
+        print("Connected by:",address)
+        data1 = conn.recv(1024)
+        data2 = conn.recv(1024)
+
+        a = int(data1.decode("utf-8"))
+        b = int(data2.decode("utf-8"))
+
+        print("Data 1 recieved from Client: ",a)
+        print("Data 2 recieved from Client: ",b)
+
+        res = a+b
+        conn.sendall(send_data(res))
+"""
+Server binded to host 127.0.0.1 and port 4698
+Connected by: ('127.0.0.1', 54959)
+Data 1 recieved from Client:  4
+Data 2 recieved from Client:  5
+
+"""
